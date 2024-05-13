5510603dfb835b7101dde94078d3250b1ee904f5
Junyu Wang <junyuwang@berkeley.edu>
2024-05-12 23:32:45 -0700

    some CLI polishing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
 use std::process;
 use std::time::Instant;
 
-use clap::Parser;
+use clap::{ArgAction, CommandFactory, Parser};
 use genson_rs::*;
 use mimalloc::MiMalloc;
 
+// Setting the global allocator to mimalloc for more efficient memory allocation
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
 
 #[derive(Parser)]
-#[command(name = "Genson")]
+#[command(name = "genson-rs")]
 #[command(version = "0.1")]
-#[command(about = "Generate one, unified JSON Schema from one or \
-more JSON objects and/or JSON Schemas. Compatible with JSON-Schema\
+#[command(about = "Generate one, unified JSON Schema from JSON objects. Compatible with JSON-Schema\
  Draft 4 and above.", long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    delimiter: Option<String>,
-
-    #[arg(short, long)]
-    encoding: Option<String>,
-
-    #[arg(short, long)]
-    schema_file: Option<String>,
+    #[arg(short, long, action=ArgAction::SetTrue)]
+    verbose: Option<bool>,
 
     json_file: Option<String>,
 }
@@ -34,16 +28,24 @@ fn main() {
 
     if let Some(json_file) = cli.json_file.as_deref() {
         let now = Instant::now();
+
         let object = parse_json_object(json_file);
-        println!("JSON Parsing took {} mili.", now.elapsed().as_millis());
+        let json_parsing_duration_ms = now.elapsed().as_millis();
 
-        let now = Instant::now();
         builder.add_object(&object);
-        println!("Schema building took {} mili.", now.elapsed().as_millis());
+        let total_duration_ms = now.elapsed().as_millis();
+        let schema_building_duration_ms = total_duration_ms - json_parsing_duration_ms;
 
-        println!("{}", builder.to_json());
+        if cli.verbose.unwrap_or(false) {
+            dbg!(json_parsing_duration_ms);
+            dbg!(schema_building_duration_ms);
+            dbg!(total_duration_ms);
+        }
+        print!("{}", builder.to_json());
         // NOTE: early exit here to avoid dropping of the `object` variable
         //  which takes about 15~35% of the total runtime (depending on the size of the object)
         process::exit(0);
+    } else {
+        <Cli as CommandFactory>::command().print_help().unwrap();
     }
 }
