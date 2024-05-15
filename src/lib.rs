5bb7c2b44784c3ab734f2cc70f214aefc2094ab3
Junyu Wang <junyuwang@berkeley.edu>
2024-05-14 20:08:54 -0700

    updated lib and verbose print

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,34 @@ mod node;
 mod strategy;
 mod builder;
 
+use std::time::Instant;
+use mimalloc::MiMalloc;
+
+// Setting the global allocator to mimalloc for more efficient memory allocation
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 pub use builder::SchemaBuilder;
-use serde_json::Value;
 
 pub fn get_builder(schema_uri: Option<&str>) -> SchemaBuilder {
     SchemaBuilder::new(schema_uri)
 }
 
-pub fn parse_json_object(json_file: &str) -> Value {
-    let object_str = std::fs::read_to_string(json_file).unwrap();
-    let object: Value = serde_json::from_str(&object_str).unwrap();
-    return object;
+/// Parse a JSON object and add it to the schema builder
+/// * `builder` - the schema builder object
+/// * `object_slice` - the JSON object to parse
+/// * `verbose_mode` - whether to print verbose output (to stderr)
+pub fn parse_json_schema(builder: &mut SchemaBuilder, object_slice: &mut Vec<u8>, verbose_mode: bool) {
+    let now = Instant::now();
+
+    let object = simd_json::to_borrowed_value(object_slice).unwrap();
+    let json_parsing_duration_ms = now.elapsed().as_millis();
+
+    builder.add_object(&object);
+    let schema_parsing_duration = now.elapsed().as_millis() - json_parsing_duration_ms;
+
+    if verbose_mode {
+        eprintln!("JSON parsing duration: {}ms", json_parsing_duration_ms);
+        eprintln!("Schema parsing duration: {}ms", schema_parsing_duration);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,7 @@ use std::process;
 use std::time::Instant;
 
 use clap::{ArgAction, CommandFactory, Parser};
-use simd_json;
 use genson_rs::*;
-use mimalloc::MiMalloc;
-
-// Setting the global allocator to mimalloc for more efficient memory allocation
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
-
 
 #[derive(Parser)]
 #[command(name = "genson-rs")]
@@ -32,22 +25,13 @@ fn main() {
 
         // let object = parse_json_object(json_file);
         let mut object_slice = std::fs::read(json_file).unwrap();
-        let file_reading_duration_ms = now.elapsed().as_millis();
-
-        let object = simd_json::to_borrowed_value(&mut object_slice).unwrap();
-        let json_parsing_duration_ms = now.elapsed().as_millis() - file_reading_duration_ms;
-
-        builder.add_object(&object);
-        let total_duration_ms = now.elapsed().as_millis();
-        let schema_building_duration_ms = total_duration_ms - json_parsing_duration_ms;
 
         if cli.verbose.unwrap_or(false) {
-            dbg!(file_reading_duration_ms);
-            dbg!(json_parsing_duration_ms);
-            dbg!(schema_building_duration_ms);
-            dbg!(total_duration_ms);
+            eprintln!("File reading duration: {}ms", now.elapsed().as_millis());
         }
-        print!("{}", builder.to_json());
+        parse_json_schema(&mut builder, &mut object_slice, cli.verbose.unwrap_or(false));
+        
+        println!("{}", builder.to_json());
         // NOTE: early exit here to avoid dropping of the `object` variable
         //  which takes about 15~35% of the total runtime (depending on the size of the object)
         process::exit(0);
